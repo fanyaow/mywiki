66d01ba0ba36cb8d4a03b505fc066aa8bee08f31
fyw <fyw>
2021-12-10 19:22:48 +0800

    mybatis

---
+++ doc/all.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test`(
+    `id`       BIGINT NOT NULL COMMENT 'id',
+    `name`     VARCHAR(32) COMMENT '名称',
+    `password` VARCHAR(32) COMMENT '密码',
+    PRIMARY KEY (`id`)
+) ENGINE = INNODB
+  DEFAULT CHARSET = utf8mb4 COMMENT '测试';
