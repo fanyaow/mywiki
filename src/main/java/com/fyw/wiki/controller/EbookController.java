b97e6c074e29912be468edc86865ba56fc820b65
Administrator <fyw>
2021-12-11 11:00:21 +0800

    增加通用返回类CommonResp

--- src/main/java/com/fyw/wiki/controller/EbookController.java
+++ src/main/java/com/fyw/wiki/controller/EbookController.java
@@ -1,7 +1,7 @@
 package com.fyw.wiki.controller;
-import com.fyw.wiki.domain.Demo;
+
 import com.fyw.wiki.domain.Ebook;
-import com.fyw.wiki.service.DemoService;
+import com.fyw.wiki.resp.CommonResp;
 import com.fyw.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +17,13 @@ public class EbookController {
     @Resource
     public EbookService ebookService;
 
-//    @GetMapping("/demo/list")
+//    @GetMapping("/ebook/list")
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list() ;
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp =new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
+
 }
