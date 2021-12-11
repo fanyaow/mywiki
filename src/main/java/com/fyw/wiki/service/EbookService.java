226005f60b6ea7732fad1f34ce61cfa356d6bb4b
fyw <fyw>
2021-12-11 15:01:21 +0800

    制作CopyUtil封装BeanUtils

--- src/main/java/com/fyw/wiki/service/EbookService.java
+++ src/main/java/com/fyw/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.fyw.wiki.domain.EbookExample;
 import com.fyw.wiki.mapper.EbookMapper;
 import com.fyw.wiki.req.EbookReq;
 import com.fyw.wiki.resp.EbookResp;
+import com.fyw.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -28,14 +29,20 @@ public class EbookService {
         criteria.andNameLike("%"+ req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-//            ebookResp.setId(ebook.getId());
-            BeanUtils.copyProperties(ebook,ebookResp);
-//            ebookResp.setId(123L);
-            respList.add(ebookResp);
-        }
-        return respList;
+//
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//
+//            //未使用工具了CopyUtil时写法
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook,ebookResp);
+//            //对象复制 使用工具类CopyUtil
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+//        return respList;
+        //列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
