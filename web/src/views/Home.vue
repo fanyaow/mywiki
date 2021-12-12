7a1d4c912330ad068db3e528c33ee011796b8db0
Administrator <fyw>
2021-12-12 22:09:53 +0800

    增加ebooks 从后端显示数据

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,66 +1,82 @@
 <template>
-    <a-layout>
-        <a-layout-sider width="200" style="background: #fff">
-            <a-menu
-                    mode="inline"
-                    v-model:selectedKeys="selectedKeys2"
-                    v-model:openKeys="openKeys"
-                    :style="{ height: '100%', borderRight: 0 }"
-            >
-                <a-sub-menu key="sub1">
-                    <template #title>
+  <a-layout>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+          mode="inline"
+          v-model:selectedKeys="selectedKeys2"
+          v-model:openKeys="openKeys"
+          :style="{ height: '100%', borderRight: 0 }"
+      >
+        <a-sub-menu key="sub1">
+          <template #title>
               <span>
-                <user-outlined />
+                <user-outlined/>
                 subnav 1
               </span>
-                    </template>
-                    <a-menu-item key="1">option1</a-menu-item>
-                    <a-menu-item key="2">option2</a-menu-item>
-                    <a-menu-item key="3">option3</a-menu-item>
-                    <a-menu-item key="4">option4</a-menu-item>
-                </a-sub-menu>
-                <a-sub-menu key="sub2">
-                    <template #title>
+          </template>
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
               <span>
-                <laptop-outlined />
+                <laptop-outlined/>
                 subnav 2
               </span>
-                    </template>
-                    <a-menu-item key="5">option5</a-menu-item>
-                    <a-menu-item key="6">option6</a-menu-item>
-                    <a-menu-item key="7">option7</a-menu-item>
-                    <a-menu-item key="8">option8</a-menu-item>
-                </a-sub-menu>
-                <a-sub-menu key="sub3">
-                    <template #title>
+          </template>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <template #title>
               <span>
-                <notification-outlined />
+                <notification-outlined/>
                 subnav 3
               </span>
-                    </template>
-                    <a-menu-item key="9">option9</a-menu-item>
-                    <a-menu-item key="10">option10</a-menu-item>
-                    <a-menu-item key="11">option11</a-menu-item>
-                    <a-menu-item key="12">option12</a-menu-item>
-                </a-sub-menu>
-            </a-menu>
-        </a-layout-sider>
-        <a-layout-content
-                :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-        >
-            Content
-        </a-layout-content>
-    </a-layout>
+          </template>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <pre>
+         {{ebooks}}
+      </pre>
+
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script lang="ts">
-    import {defineComponent} from 'vue';
-    import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {defineComponent, onMounted,ref} from 'vue';
+import axios from 'axios';
 
-    export default defineComponent({
-        name: 'Home',
-        components: {
-            HelloWorld,
-        },
-    });
+export default defineComponent({
+  name: 'Home',
+  setup() {
+    console.log("setup");
+    const ebooks =ref();
+
+    onMounted(() => {
+      console.log('onMounted');
+      axios.get("http://localhost:8000/ebook/list?name=Spring").then((response) => {
+        const data = response.data;
+        ebooks.value= data.content;
+        console.log(response)
+      });
+    })
+    return {
+      ebooks
+    }
+  }
+})
 </script>
