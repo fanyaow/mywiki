cf8fa4c2687b984d8289eba78f0d33681c776f71
Administrator <fyw>
2021-12-18 12:11:01 +0800

    增加全局axios 配置

--- web/src/main.ts
+++ web/src/main.ts
@@ -10,11 +10,13 @@ import axios from "axios";
 //增加全局配置
 axios.defaults.baseURL=process.env.VUE_APP_SERVER
 
+
 /**
  * axios拦截器
  */
 axios.interceptors.request.use(function (config) {
     console.log('请求参数：', config);
+
     return config;
 }, error => {
     return Promise.reject(error);
@@ -23,11 +25,9 @@ axios.interceptors.response.use(function (response) {
     console.log('返回结果：', response);
     return response;
 }, error => {
-    console.log('返回错误：', error);
-
-    return Promise.reject(error);
+    console.log('返回错误:',error)
+    return Promise.reject(error)
 });
-
 
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
