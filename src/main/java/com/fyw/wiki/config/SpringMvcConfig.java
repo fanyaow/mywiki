4d18e5caf9e9723a0e1d0acdb9e38c69896becc2
Administrator <fyw>
2021-12-18 12:59:57 +0800

    增加AOP 打印接口耗时,请求参数和返回参数 去掉过滤器fiter ,拦截器interceptor

--- src/main/java/com/fyw/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/fyw/wiki/config/SpringMvcConfig.java
@@ -1,24 +1,24 @@
-package com.fyw.wiki.config;
-
-import com.fyw.wiki.interceptor.LoginInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LoginInterceptor loginInterceptor;
-
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-
-
-
-    }
-}
+//package com.fyw.wiki.config;
+//
+//import com.fyw.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor loginInterceptor;
+//
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//
+//
+//
+//    }
+//}
