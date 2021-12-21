08a922acddbfee7bf3c92105ab514df50b5253bd
Administrator <fyw>
2021-12-21 22:35:27 +0800

    增加tool工具,编辑时复制对象,修改表单时,不会影响列表数据

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
     'vue/no-unused-components':'off',
     '@typescript-eslint/no-explicit-any': 0,
     'vue/no-unused-vars':'off',
-    '@typescript-eslint/no-unused-vars':'off'
+    '@typescript-eslint/no-unused-vars':'off',
+    "@typescript-eslint/ban-types":0,
+    "@typescript-eslint/explicit-module-boundary-types":0,
   }
 }
