905e402c827d23da51e1e99a552e70cc33a789bc
Administrator <fyw>
2021-12-23 22:51:16 +0800

    首页显示分类

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -7,12 +7,10 @@
           @click = "handleClick"
       >
         <a-menu-item key="welcome">
-          <router-link :to="'/'">
             <MailOutlined />
             <span>欢迎</span>
-          </router-link>
         </a-menu-item>
-        <a-sub-menu v-for="item in level1" :key="item.id" :disabled="true">
+        <a-sub-menu v-for="item in level1" :key="item.id" :disabled="false">
           <template v-slot:title>
             <span><user-outlined />{{item.name}}</span>
           </template>
@@ -28,7 +26,10 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-list item-layout="vertical" size="large"  :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+      <div class="welcome" v-show="isShowWelcome">
+        <h1>欢迎来到我的wiki</h1>
+      </div>
+      <a-list v-show="!isShowWelcome" item-layout="vertical" size="large"  :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
@@ -96,9 +97,20 @@ export default defineComponent({
         }
       });
     };
-    const handleClick=()=>{
-      console.log("menu click")
+
+    const isShowWelcome =ref(true);
+
+    const handleClick = (value: any) => {
+      // console.log("menu click", value)
+      if (value.key === 'welcome') {
+        isShowWelcome.value = true;
+      } else {
+        isShowWelcome.value = false;
+      }
+      // isShowWelcome.value = value.key === 'welcome';
     };
+
+
     onMounted(() => {
       handleQueryCategory();
       // console.log('onMounted');
@@ -135,6 +147,7 @@ export default defineComponent({
 
       handleClick,
       level1,
+      isShowWelcome,
     }
   }
 })
