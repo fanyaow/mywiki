5827f8a0c7cc3919035a2042c74734013741c876
Administrator <fyw>
2021-12-23 20:24:16 +0800

    父分类改为下拉框

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -59,12 +59,18 @@
         <a-input v-model:value="category.name" />
       </a-form-item>
       <a-form-item label="父分类">
-<!--        <a-cascader-->
-<!--            v-model:value="category.parent"-->
-<!--            :field-names="{ label: 'name', value: 'id', children: 'children' }"-->
-<!--            :options="level1"-->
-<!--        />-->
-        <a-input v-model:value="category.parent" />
+<!--        <a-input v-model:value="category.parent" />-->
+        <a-select
+            v-model:value="category.parent"
+            ref="select"
+        >
+          <a-select-option :value="0">
+            无
+          </a-select-option>
+          <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="category.id === c.id">
+            {{c.name}}
+          </a-select-option>
+        </a-select>
       </a-form-item>
       <a-form-item label="顺序">
         <a-input v-model:value="category.sort" />
