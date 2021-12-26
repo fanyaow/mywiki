725ed744e369f44c905c6462bbe178610704efda
Administrator <fyw>
2021-12-26 16:51:35 +0800

    文档管理表格默认展开所有

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -21,14 +21,14 @@
 
           </p>
           <a-table
-
+              v-if="level1.length > 0"
               :columns="columns"
               :row-key="record => record.id"
               :data-source="level1"
               :pagination="false"
               :loading="loading"
               size="small"
-
+              :defaultExpandAllRows="true"
           >
             <template #name="{ text, record }">
               {{record.sort}} {{text}}
