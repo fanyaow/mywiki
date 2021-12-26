51811abe8af3f01072ee16d1333dde54bb0d623e
Administrator <fyw>
2021-12-26 15:12:00 +0800

    在文档表中显示富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -46,6 +46,7 @@
           </a-space>
         </template>
       </a-table>
+
     </a-layout-content>
   </a-layout>
   <a-modal
@@ -61,18 +62,21 @@
       <a-form-item label="父分类">
 
         <a-tree-select
-                v-model:value="doc.parent"
-                style="width: 100%"
-                :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
-                :tree-data="treeSelectData"
-                placeholder="请选择父文档"
-                tree-default-expand-all
-                :replaceFields ="{title : 'name' , key:'id',value:'id'}"
+            v-model:value="doc.parent"
+            style="width: 100%"
+            :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+            :tree-data="treeSelectData"
+            placeholder="请选择父文档"
+            tree-default-expand-all
+            :replaceFields ="{title : 'name' , key:'id',value:'id'}"
         >
         </a-tree-select>
       </a-form-item>
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort" />
+      </a-form-item>
+      <a-form-item label="内容">
+        <div id="content"></div>
       </a-form-item>
     </a-form>
   </a-modal>
@@ -80,259 +84,269 @@
 
 <script lang="ts">
 import {createVNode, defineComponent, onMounted, ref} from "vue";
-  import axios from 'axios';
-  import {message, Modal} from "ant-design-vue";
-  import {Tool} from "@/util/tool";
-  import {useRoute} from "vue-router";
+import axios from 'axios';
+import {message, Modal} from "ant-design-vue";
+import {Tool} from "@/util/tool";
+import {useRoute} from "vue-router";
 import {ExclamationCircleOutlined} from "@ant-design/icons-vue";
+import E from 'wangeditor';
 
-  export default defineComponent({
-    name:'AdminDoc',
-    setup(){
+export default defineComponent({
+  name:'AdminDoc',
+  setup(){
 
-      const route=useRoute();
-      console.log("路由:",route);
-      console.log("route.path：", route.path);
-      console.log("route.query：", route.query);
-      console.log("route.param：", route.params);
-      console.log("route.fullPath：", route.fullPath);
-      console.log("route.name：", route.name);
-      console.log("route.meta：", route.meta);
+    const route=useRoute();
+    console.log("路由:",route);
+    console.log("route.path：", route.path);
+    console.log("route.query：", route.query);
+    console.log("route.param：", route.params);
+    console.log("route.fullPath：", route.fullPath);
+    console.log("route.name：", route.name);
+    console.log("route.meta：", route.meta);
 
 
 
-      const param =ref();
-      param.value={};
-      const docs=ref();
+    const param =ref();
+    param.value={};
+    const docs=ref();
 
-      const loading =ref(false);
+    const loading =ref(false);
 
-      const columns=[
+    const columns=[
 
-        {
-          title: '名称',
-          dataIndex: 'name'
-        },
-        {
-          title: '父分类',
-          dataIndex: 'parent'
-        },
-        {
-          title: '排序',
-          dataIndex: 'sort'
-        },
-        {
-          title: '操作',
-          key: 'action',
-          slots: { customRender: 'action' }
-        }
-      ];
+      {
+        title: '名称',
+        dataIndex: 'name'
+      },
+      {
+        title: '父分类',
+        dataIndex: 'parent'
+      },
+      {
+        title: '排序',
+        dataIndex: 'sort'
+      },
+      {
+        title: '操作',
+        key: 'action',
+        slots: { customRender: 'action' }
+      }
+    ];
 
-      /**
-       * 一级分类树，children属性就是二级分类
-       * [{
-       *   id: "",
-       *   name: "",
-       *   children: [{
-       *     id: "",
-       *     name: "",
-       *   }]
-       * }]
-       */
-      const level1 = ref(); // 一级分类树，children属性就是二级分类
+    /**
+     * 一级分类树，children属性就是二级分类
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1 = ref(); // 一级分类树，children属性就是二级分类
+    level1.value = [];
+
+    /**
+     * 数据查询
+     **/
+    const handleQuery = () => {
+      loading.value = true;
+      // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+
       level1.value = [];
+      axios.get("/doc/all").then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if(data.success){
+          docs.value=data.content;
+          console.log("原始数组：", docs.value);
 
-      /**
-       * 数据查询
-       **/
-      const handleQuery = () => {
-        loading.value = true;
-        // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+          level1.value = [];
+          level1.value = Tool.array2Tree(docs.value, 0);
+          console.log("树形结构：", level1);
 
-        level1.value = [];
-        axios.get("/doc/all").then((response) => {
-          loading.value = false;
-          const data = response.data;
-          if(data.success){
-            docs.value=data.content;
-            console.log("原始数组：", docs.value);
-
-            level1.value = [];
-            level1.value = Tool.array2Tree(docs.value, 0);
-            console.log("树形结构：", level1);
-
-          }else {
-            message.error(data.message)
-          }
-        });
-      };
-
-
-      //表单
-      // 因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独声明一个响应式变量
-      const treeSelectData=ref();
-      treeSelectData.value=[];
-
-      const  doc = ref();
-      const modalVisible = ref(false);
-      const modalLoading = ref(false);
-      const handleModalOk = ()=>{
-        modalLoading.value=true;
-        axios.post("/doc/save", doc.value).then((response) => {
-          modalLoading.value = false;
-          const data = response.data;
-          if (data.success) {
-            modalVisible.value = false;
-            //重新加载列表
-            handleQuery();
-          }else{
-            message.error(data.message)
-          }
-        });
-      };
-
-      /**
-       * 将某节点及其子孙节点全部置为disabled
-       */
-      const setDisable = (treeSelectData: any, id: any) => {
-        // console.log(treeSelectData, id);
-        // 遍历数组，即遍历某一层节点
-        for (let i = 0; i < treeSelectData.length; i++) {
-          const node = treeSelectData[i];
-          if (node.id === id) {
-            // 如果当前节点就是目标节点
-            console.log("disabled", node);
-            // 将目标节点设置为disabled
-            node.disabled = true;
-
-            // 遍历所有子节点，将所有子节点全部都加上disabled
-            const children = node.children;
-            if (Tool.isNotEmpty(children)) {
-              for (let j = 0; j < children.length; j++) {
-                setDisable(children, children[j].id)
-              }
-            }
-          } else {
-            // 如果当前节点不是目标节点，则到其子节点再找找看。
-            const children = node.children;
-            if (Tool.isNotEmpty(children)) {
-              setDisable(children, id);
-            }
-          }
+        }else {
+          message.error(data.message)
         }
-      };
-
-
-      const deleteIds: Array<string> = [];
-      const deleteNames: Array<string> = [];
-      /**
-       * 查找整根树枝
-       */
-      const getDeleteIds = (treeSelectData: any, id: any) => {
-        // console.log(treeSelectData, id);
-        // 遍历数组，即遍历某一层节点
-        for (let i = 0; i < treeSelectData.length; i++) {
-          const node = treeSelectData[i];
-          if (node.id === id) {
-            // 如果当前节点就是目标节点
-            console.log("delete", node);
-            // 将目标ID放入结果集ids
-            // node.disabled = true;
-            deleteIds.push(id);
-            deleteNames.push(node.name);
-
-            // 遍历所有子节点
-            const children = node.children;
-            if (Tool.isNotEmpty(children)) {
-              for (let j = 0; j < children.length; j++) {
-                getDeleteIds(children, children[j].id)
-              }
-            }
-          } else {
-            // 如果当前节点不是目标节点，则到其子节点再找找看。
-            const children = node.children;
-            if (Tool.isNotEmpty(children)) {
-              getDeleteIds(children, id);
-            }
-          }
-        }
-      };
-
-      //修改
-      const edit =(record : any)=>{
-        modalVisible.value=true;
-        doc.value=Tool.copy(record);
-
-        // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
-        treeSelectData.value = Tool.copy(level1.value);
-        setDisable(treeSelectData.value, record.id);
-
-        // 为选择树添加一个"无" unpush 是往后边添加一个 无
-        treeSelectData.value.unshift({id: 0, name: '无'});
-      };
-      //新增
-      const add =()=>{
-        modalVisible.value=true;
-        doc.value= {
-          ebookId:route.query.ebookId,
-        };
-
-        treeSelectData.value = Tool.copy(level1.value) || [];
-
-        // 为选择树添加一个"无"
-        treeSelectData.value.unshift({id: 0, name: '无'});
-      };
-
-      const handleDelete = (id: number)=> {
-        // 清空数组，否则多次删除时，数组会一直增加
-        deleteIds.length = 0;
-        deleteNames.length=0;
-        getDeleteIds(level1.value, id);
-        Modal.confirm({
-          title: '重要提醒',
-          icon: createVNode(ExclamationCircleOutlined),
-          content: '将删除：【' + deleteNames.join("，") + "】删除后不可恢复，确认删除？",
-          onOk() {
-
-            axios.delete("/doc/delete/" + deleteIds.join(",")).then((response) => {
-              const data = response.data;
-              if (data.success) {
-                //重新加载列表
-                handleQuery();
-              }else {
-                message.error(data.message);
-              }
-            });
-          },
-        });
-      }
-
-
-      onMounted(()=>{
-        handleQuery();
       });
+    };
 
-      return {
-        param,
-        // docs,
-        level1,
-        columns,
-        loading,
-        handleQuery,
 
-        edit,
-        add,
+    //表单
+    // 因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独声明一个响应式变量
+    const treeSelectData=ref();
+    treeSelectData.value=[];
+    const  doc = ref();
+    const modalVisible = ref(false);
+    const modalLoading = ref(false);
+    const editor =new E('#content');
 
-        doc,
-        modalVisible,
-        modalLoading,
-        handleModalOk,
 
-        handleDelete,
-        treeSelectData,
+    const handleModalOk = ()=>{
+      modalLoading.value=true;
+      axios.post("/doc/save", doc.value).then((response) => {
+        modalLoading.value = false;
+        const data = response.data;
+        if (data.success) {
+          modalVisible.value = false;
+          //重新加载列表
+          handleQuery();
+        }else{
+          message.error(data.message)
+        }
+      });
+    };
+
+    /**
+     * 将某节点及其子孙节点全部置为disabled
+     */
+    const setDisable = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("disabled", node);
+          // 将目标节点设置为disabled
+          node.disabled = true;
+
+          // 遍历所有子节点，将所有子节点全部都加上disabled
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              setDisable(children, children[j].id)
+            }
+          }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            setDisable(children, id);
+          }
+        }
       }
+    };
+
+
+    const deleteIds: Array<string> = [];
+    const deleteNames: Array<string> = [];
+    /**
+     * 查找整根树枝
+     */
+    const getDeleteIds = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("delete", node);
+          // 将目标ID放入结果集ids
+          // node.disabled = true;
+          deleteIds.push(id);
+          deleteNames.push(node.name);
+
+          // 遍历所有子节点
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              getDeleteIds(children, children[j].id)
+            }
+          }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            getDeleteIds(children, id);
+          }
+        }
+      }
+    };
+
+    //修改
+    const edit =(record : any)=>{
+      modalVisible.value=true;
+      doc.value=Tool.copy(record);
+
+      // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
+      treeSelectData.value = Tool.copy(level1.value);
+      setDisable(treeSelectData.value, record.id);
+
+      // 为选择树添加一个"无" unpush 是往后边添加一个 无
+      treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        editor.create();
+      },100);
+    };
+    //新增
+    const add =()=>{
+      modalVisible.value=true;
+      doc.value= {
+        ebookId:route.query.ebookId,
+      };
+
+      treeSelectData.value = Tool.copy(level1.value) || [];
+
+      // 为选择树添加一个"无"
+      treeSelectData.value.unshift({id: 0, name: '无'});
+      setTimeout(function (){
+        editor.create();
+      },100);
+    };
+
+    const handleDelete = (id: number)=> {
+      // 清空数组，否则多次删除时，数组会一直增加
+      deleteIds.length = 0;
+      deleteNames.length=0;
+      getDeleteIds(level1.value, id);
+      Modal.confirm({
+        title: '重要提醒',
+        icon: createVNode(ExclamationCircleOutlined),
+        content: '将删除：【' + deleteNames.join("，") + "】删除后不可恢复，确认删除？",
+        onOk() {
+
+          axios.delete("/doc/delete/" + deleteIds.join(",")).then((response) => {
+            const data = response.data;
+            if (data.success) {
+              //重新加载列表
+              handleQuery();
+            }else {
+              message.error(data.message);
+            }
+          });
+        },
+      });
     }
-  })
+
+
+    onMounted(()=>{
+
+      handleQuery();
+    });
+
+    return {
+      param,
+      // docs,
+      level1,
+      columns,
+      loading,
+      handleQuery,
+
+      edit,
+      add,
+
+      doc,
+      modalVisible,
+      modalLoading,
+      handleModalOk,
+
+      handleDelete,
+      treeSelectData,
+    }
+  }
+})
 </script>
 <style scoped>
 img {
