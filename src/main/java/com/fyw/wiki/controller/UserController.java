2169a68eca0caffbe8012d75d737b2c9e0f0cb32
fyw <fyw>
2021-12-31 17:15:49 +0800

    9.9 集成redis  登录成功后将登录信息放入到redis中

--- src/main/java/com/fyw/wiki/controller/UserController.java
+++ src/main/java/com/fyw/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.fyw.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.fyw.wiki.req.UserLoginReq;
 import com.fyw.wiki.req.UserQueryReq;
 import com.fyw.wiki.req.UserResetPasswordReq;
@@ -9,18 +10,30 @@ import com.fyw.wiki.resp.PageResp;
 import com.fyw.wiki.resp.UserLoginResp;
 import com.fyw.wiki.resp.UserQueryResp;
 import com.fyw.wiki.service.UserService;
+import com.fyw.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     public UserService userService;
+    @Resource  //jdk的
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
 //    @GetMapping("/user/list")
     @GetMapping("/list")
@@ -60,8 +73,15 @@ public class UserController {
     @PostMapping("/login")
     public CommonResp login(@Valid @RequestBody UserLoginReq req) {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
-        CommonResp resp = new CommonResp<>();
+        CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token =snowFlake.nextId();
+        LOG.info("生成单点登录token：{}，并放入redis中", token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token.toString(), JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
+
         resp.setContent(userLoginResp);
         return resp;
     }
