71a7071455909c0303ffb2d2eafd4fc05004bdde
Administrator <fyw>
2022-01-03 15:52:18 +0800

    10-11 点赞功能使用异步通知即可,去掉RocketMq的相关代码配置

--- src/main/java/com/fyw/wiki/service/DocService.java
+++ src/main/java/com/fyw/wiki/service/DocService.java
@@ -20,7 +20,6 @@ import com.fyw.wiki.util.SnowFlake;
 import com.fyw.wiki.websocket.WebSocketServer;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -58,8 +57,8 @@ public class DocService {
     @Resource
     private WsService wsService;
 
-    @Resource
-    public RocketMQTemplate rocketMQTemplate;
+//    @Resource
+//    public RocketMQTemplate rocketMQTemplate;
 
     public List<DocQueryResp> all(Long ebookId) {
         DocExample docExample = new DocExample();
@@ -167,7 +166,7 @@ public class DocService {
         // 远程IP+doc.id作为key，24小时内不能重复
         String ip = RequestContext.getRemoteAddr();
 
-        if (redisUtil.validateRepeat("DOC_VOTE_" + id + "_" + ip, 5000)) {
+        if (redisUtil.validateRepeat("DOC_VOTE_" + id + "_" + ip, 3600*24)) {
             docMapperCust.updateDocVoteCount(id);
         } else {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
@@ -176,9 +175,10 @@ public class DocService {
         //推送消息
         Doc docDb = docMapper.selectByPrimaryKey(id);
         String logId= MDC.get("LOG_ID");
-//        wsService.sendInfo("["+docDb.getName()+"]被点赞!",logId);
+        wsService.sendInfo("["+docDb.getName()+"]被点赞!",logId);
 
-        rocketMQTemplate.convertAndSend("VOTE_TOPIC", "【" + docDb.getName() + "】被点赞！");
+        //使用rocketMQ 接受消息
+//        rocketMQTemplate.convertAndSend("VOTE_TOPIC", "【" + docDb.getName() + "】被点赞！");
     }
 
     //定时更新ebook里边的相关数据
